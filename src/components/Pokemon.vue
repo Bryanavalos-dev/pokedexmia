57fff4699dd12946efc93ff4d6ee5ec3209e1688
Bryanavalos-dev <73485085+Bryanavalos-dev@users.noreply.github.com>
2020-10-29 11:01:30 -0600

    Add files via upload

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -13,11 +13,11 @@
   </div> -->
   
   <!-- contenedor principal -->
-  <div class="flex justify-center">
+  <div class="flex justify-center h-screen">
    <!-- contenedor centrado -->
-    <div class="celular:w-screen font-body relative  h-screen flex-col flex justify-between" :class="`bg-${$route.params.color}-400`">
+    <div class="celular:w-screen font-body absolute items-center h-screen flex-col flex justify-between" :class="`bg-${$route.params.color}-400`">
       <!-- contenedor inforarriba -->
-      <div class="w-full h-64 flex flex-col p-4">
+      <div class="w-full h-1/2 flex flex-col p-4">
         <!-- contenedorflechacorazon -->
 
         <div class=" flex w-full h-8  justify-between">
@@ -48,14 +48,14 @@
         
       </div>
       <!-- contenedor imagen -->
-      <div class="flex justify-center absolute top-50 left-45">
+      <div class="flex justify-center relative inset-0 top-alto left-0">
           <div class="w-48 h-48 flex  ">
             <img :src="`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`" class="h-full w-full">
           </div>      
          </div>
       <!-- contenedor inforabajo -->
-      <div class="w-screen rounded-t-3xl  h-11/20 bg-white justify-around ">
-         <div class="flex flex-row mt-6 justify-around">
+      <div class="w-screen rounded-t-3xl  h-11/20 bg-white ">
+         <div class="flex flex-row mt-5 justify-around">
            <button @click="tab = 'about'">About</button>
            <button @click="tab = 'base'"> Base Stats</button>
            <button enable=false>Evolution</button>
@@ -64,7 +64,7 @@
          
          <div v-if="tab == 'about'" class="flex flex-col justify-around">
               <!-- informacion -->
-              <div class="flex flex-col m-4 space-y-4">
+              <div class="flex flex-col m-3 space-y-2">
                 <!-- datos -->
                 
                   <div class="flex items-center space-x-12"><span class="text-gray-600">Species</span><span>{{pokemon.stats[5].stat.name}}</span></div>
@@ -77,7 +77,7 @@
               <!-- breding -->
               <span class="ml-4 font-bold">BREADING</span>
               <!-- informacion -->
-              <div class="flex flex-col m-4 space-y-4">
+              <div class="flex flex-col m-4 space-y-2">
                 <!-- datos -->
                 
                   <div class="flex items-center space-x-16"><span class="text-gray-600">Gender</span><span>{{pokemon.stats[5].stat.name}}</span></div>
